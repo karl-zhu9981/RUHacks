46d519a45f4cbce408ad9837d0baea29d436b1b2
Connor Horman <chorman64@gmail.com>
2020-05-15 20:39:20 -0400

    Start test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene,decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+pub fn root() -> &'static str{
+    "Hello World"
+}
+
+pub fn main(){
+    rocket::ignite().mount("/",routes![root]).launch();
 }
